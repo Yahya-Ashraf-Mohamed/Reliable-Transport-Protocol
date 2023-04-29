674f6a0d9179dfe03d75af4c3873c8364d2e4d6b
Yahya-Ashraf-Mohamed <s-yahya.mohamed@zewailcity.edu.eg>
2023-04-29 23:49:33 +0300

    Updating project V1.1

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
+import socket
+import time
 import struct
+import os
 
+# function to convert bytes to integer
+def bytes_to_int(b):
+    return int.from_bytes(b, byteorder='big')
 
 # function to convert bites to bytes
 def bitesIntobytes(i, bitDigit):
@@ -11,18 +16,9 @@ def bitesIntobytes(i, bitDigit):
         binary_str = '{0:032b}'.format(i)
         return bytes(int(binary_str[i:i + 8], 2) for i in range(0, len(binary_str), 8))
 
-
-# =========================================================================================
-def TrailerValue(chunk, i):
-    if i != len(chunk) - 1:
-        return bitesIntobytes(0x0000, 32)
-    else:
-        return bitesIntobytes(0xFFFF, 32)
-
-
-# =========================================================================================
-def DevidingPictureIntoByties(picturePath, max_chunk_size):
-    with open(picturePath, "rb") as img_file:
+# function to divide picture into packets
+def divide_picture_into_packets(picture_path, max_chunk_size):
+    with open(picture_path, "rb") as img_file:
         img_data = img_file.read()
         chunks = []
         max_chunk_bytes = max_chunk_size // 8
@@ -34,23 +30,135 @@ def DevidingPictureIntoByties(picturePath, max_chunk_size):
     return chunks
 
 
-# =========================================================================================
-def AddingHeadersToThePackets(picturepath, File_id):
-    chunk = DevidingPictureIntoByties(picturepath, max_chunk_size)
-    Packets = []
-    for i in range(len(chunk)):
-        packetId = bitesIntobytes(i, 16)
-        Trailer = TrailerValue(chunk, i)
-        packet = packetId + File_id + chunk[i] + Trailer
-        Packets.append(packet)
-    print(Packets[0])
+# function to add headers to the packets
+def add_headers_to_packets(picture_path, file_id, max_chunk_size):
+    chunks = divide_picture_into_packets(picture_path, max_chunk_size)
+    packets = []
+    for i in range(len(chunks)):
+        packet_id = bitesIntobytes(i, 16)
+        trailer = get_trailer_value(chunks, i)
+        packet = packet_id + file_id + chunks[i] + trailer
+        packets.append(packet)
+    return packets
 
 
-# =========================================================================================
+# function to get trailer value for a packet
+def get_trailer_value(chunks, i):
+    if i != len(chunks) - 1:
+        return bitesIntobytes(0x0000, 32)
+    else:
+        return bitesIntobytes(0xFFFF, 32)
 
 
-max_chunk_size = 1024  # maximum massage size
-Vindow_size = 4  # sliding window size in go back N protocol
-File_id = bitesIntobytes(0, 16)
+# function to create socket and bind to a port
+def create_socket(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('127.0.0.1', port))
+    return s
 
-AddingHeadersToThePackets('SmallFile.png', File_id)
+
+def send_packets_to_receiver(packets, window_size, timeout):
+    sock = create_socket(9000)
+    expected_ack = 0
+    start_time = time.time()
+    unacknowledged_packets = packets[:window_size]
+    
+    while unacknowledged_packets:
+        for i in range(len(unacknowledged_packets)):
+            sock.sendto(unacknowledged_packets[i], ('127.0.0.1', 8000))
+            
+        try:
+            sock.settimeout(timeout)
+            ack, addr = sock.recvfrom(1024)
+            received_ack = struct.unpack('H', ack[:2])[0]
+            
+            if received_ack >= expected_ack:
+                num_acked_packets = received_ack - expected_ack + 1
+                expected_ack += num_acked_packets
+                unacknowledged_packets = packets[expected_ack:expected_ack+window_size]
+                
+        except socket.timeout:
+            unacknowledged_packets = packets[expected_ack:expected_ack+window_size]
+            for i in range(len(unacknowledged_packets)):
+                sock.sendto(unacknowledged_packets[i], ('127.0.0.1', 8000))
+    
+    elapsed_time = time.time() - start_time
+    
+    # ask user if they want to send another file
+    while True:
+        choice = input("Do you want to send another file? (Y/N) ")
+        if choice.lower() == "y":
+            return True
+        elif choice.lower() == "n":
+            return False
+        else:
+            print("Invalid input, please enter 'Y' or 'N'.")
+
+
+# function to receive packets from sender
+def receive_packets_from_sender(expected_packets_num, timeout):
+    sock = create_socket(8000)
+    packets = []
+    last_ack = 0
+    while len(packets) < expected_packets_num:
+        try:
+            sock.settimeout(timeout)
+            data, addr = sock.recvfrom(1024)
+            packet_id = struct.unpack('H', data[:2])[0]
+            if packet_id == last_ack + 1:
+                packets.append(data)
+                last_ack += 1
+                ack = struct.pack('H', last_ack)
+                sock.sendto(ack, ('127.0.0.1', 9000))
+            else:
+                ack = struct.pack('H', last_ack)
+                sock.sendto(ack, ('127.0.0.1', 9000))
+        except socket.timeout:
+            continue
+    # Acknowledge the last packet
+    ack = struct.pack('H', last_ack)
+    sock.sendto(ack, ('127.0.0.1', 9000))
+    return packets
+
+# function to extract data from packets
+def extract_data_from_packets(packets):
+    data = b''
+    for packet in packets:
+        data += packet[4:-4]
+    return data
+
+# function to save data as image file
+def save_data_as_image(data, output_file):
+    with open(output_file, "wb") as img_file:
+        img_file.write(data)
+
+# main function to receive and save file
+def receive_file(expected_packets_num, timeout, output_file):
+    packets = receive_packets_from_sender(expected_packets_num, timeout)
+    data = extract_data_from_packets(packets)
+    save_data_as_image(data, output_file)
+
+
+# main function
+def main():
+    picture_path = 'D:\Self Development\Zewail collage material\Academic years\Year 3\Semester 2\Computer Networks\Project\ChatGPT\SmallFile.png'
+    file_id = bitesIntobytes(12345678, 32)
+    max_chunk_size = 1024
+    window_size = 5
+    timeout = 3
+
+    packets = add_headers_to_packets(picture_path, file_id, max_chunk_size)
+    elapsed_time = send_packets_to_receiver(packets, window_size, timeout)
+
+    expected_packets_num = len(packets)
+    packets = receive_packets_from_sender(expected_packets_num, timeout)
+
+    data = extract_data_from_packets(packets)
+    output_file = 'output.png'
+    save_data_as_image(data, output_file)
+
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
+if __name__ == '__main__':
+    main()
+
