6437af0fae3cfabaf350bc26609b7419f30dbf33
fatma taha <94760329+fatmaT2001@users.noreply.github.com>
2023-04-30 04:00:07 +0300

    .......

---
+++ Receiver.py
@@ -0,0 +1,75 @@
+# function to receive packets from sender
+import socket
+
+
+# =========================================================================================
+def AckId(RecivedMassage):
+    RecivedMassageId = RecivedMassage[:2]
+    packetId = int.from_bytes(RecivedMassageId, byteorder='big')
+    return packetId
+
+
+# =========================================================================================
+def AckFileId(RecivedMassage):
+    RecivedMassageId = RecivedMassage[2:]
+    PacketFileId = int.from_bytes(RecivedMassageId, byteorder='big')
+    return PacketFileId
+
+
+# =========================================================================================
+def create_socket(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('127.0.0.1', port))
+    return s
+
+
+# =========================================================================================
+def save_data_as_image(data, output_file):
+    with open(output_file, "wb") as img_file:
+        img_file.write(data)
+
+
+# =========================================================================================
+def Write_Data(filename, data_buffer):
+    with open('received_data.txt', 'wb') as f:
+        for data in data_buffer:
+            f.write(data)
+
+
+# =========================================================================================
+port = 9000
+r = create_socket(port)
+# Define the expected packet ID and create a buffer to store received data
+expected_packet_id = 0
+data_buffer = []
+# Define the window size
+window_size = 4
+filename = ''
+# Loop to receive packets
+while True:
+    # Receive a packet
+    packet, client_address = sock.recvfrom(1024)
+    # Parse the packet to extract the header and trailer information and the application data
+    packet_header = packet[:4]  # Assuming a 4-byte header
+    if AckId(packet_header) == 0:
+        file_id_initial=AckFileId(packet_header)
+    file_id = AckFileId(packet_header)
+    packet_data = packet[4:-4]  # Assuming a trailer of 4 bytes
+    packet_trailer = packet[-6:]  # Assuming a 4-byte trailer
+    # Extract the packet ID from the header
+    packet_id = AckId(packet_header)
+    # If the packet ID is the expected ID, store the data in the buffer
+    if packet_id == expected_packet_id and file_id==file_id_initial:
+        data_buffer.append(packet_data)
+        expected_packet_id += 1
+        sock.sendto(packet_header, client_address)
+    else:
+        print(f"Received out of order packet {packet_id}. Discarding data.")
+        if packet_trailer == b'\xff\xff':
+            Write_Data(filename, data_buffer)
+            print("File reception complete.")
+
+# Write the received data to a file
+
+
+# Indicate via the user interface that the file reception is complete
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import os
 import struct
+import socket
+from queue import Queue
 
 
+# =========================================================================================
+def AckId(RecivedMassage):
+    RecivedMassageId = RecivedMassage[:2]
+    packetId = int.from_bytes(RecivedMassageId, byteorder='big')
+    return packetId
+
+
+# =========================================================================================
+def AckFileId(RecivedMassage):
+    RecivedMassageId = RecivedMassage[2:]
+    PacketFileId = int.from_bytes(RecivedMassageId, byteorder='big')
+    return PacketFileId
+
+
+# =========================================================================================
 # function to convert bites to bytes
 def bitesIntobytes(i, bitDigit):
     if bitDigit == 16:
@@ -43,14 +60,60 @@ def AddingHeadersToThePackets(picturepath, File_id):
         Trailer = TrailerValue(chunk, i)
         packet = packetId + File_id + chunk[i] + Trailer
         Packets.append(packet)
-    print(Packets[0])
+    return Packets
 
 
 # =========================================================================================
+def create_socket(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('127.0.0.1', port))
+    return s
 
 
+# =========================================================================================
+# function to send packets to receiver
+def send_packets_to_receiver(packets, window_size, timeout, file_id):
+    sock = create_socket(9000)
+    unack_packets = packets[:window_size]
+    expectedIds = [AckId(i) for i in window_size]
+    while unack_packets[0]:
+        # first [p(0),p(1),p(2),p(3)]
+        #       [p1id,p2id,p3id,p4id]
+        for i in range(len(unack_packets)):
+            if i == 0:
+                sock.settimeout(timeout)
+            sock.sendto(unack_packets[i], ('127.0.0.1', 8000))
+        try:
+            ack, addr = sock.recvfrom(1024)
+            received_ack_id = AckId(ack)
+            if received_ack_id in expectedIds and file_id == AckFileId(
+                    ack):  # if received_ack id within the expected id and the received file id is
+                # corrct
+                expectedIds = [i for i in range(received_ack_id + 1, 4 + (
+                        received_ack_id + 1))]  # update the  list of expected ids depending on the received_one
+                for i in range(received_ack_id + 1):  # update the last of unacknowledged
+                    unack_packets.pop(i)
+                if len(packets) > window_size:  # checking wither the packets contains less than the window size
+                    for i in range(received_ack_id + 1, 4 + (received_ack_id + 1)):
+                        unack_packets.append(packets[i])
+                else:
+                    # the case when the remaining packets is less that the window size
+                    for i in range(received_ack_id + 1, len(packets) + 1 + received_ack_id):
+                        unack_packets.append(None)
+                packets = packets[received_ack_id + 1:]
+
+        except socket.timeout:
+            unack_packets = packets[:window_size]
+
+
+# =========================================================================================
 max_chunk_size = 1024  # maximum massage size
-Vindow_size = 4  # sliding window size in go back N protocol
+window_size = 4  # sliding window size in go back N protocol
 File_id = bitesIntobytes(0, 16)
-
-AddingHeadersToThePackets('SmallFile.png', File_id)
+flag='yes'
+while flag=='yes':
+    File_name = input("enter the file name")
+    packets = AddingHeadersToThePackets(File_name, File_id)
+    send_packets_to_receiver(File_name, window_size, 5, file_id)
+    File_id+=1
+    flag=input("Do you want to send another file")
