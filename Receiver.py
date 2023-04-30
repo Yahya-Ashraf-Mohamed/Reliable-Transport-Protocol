479ef483aec56059478efbe7c4ea36b169eaa04b
Moustafa Ashamwy <94708036+mouashmawy@users.noreply.github.com>
2023-04-30 20:19:01 +0200

    client v1.2

--- Receiver.py
+++ Receiver.py
@@ -16,12 +16,12 @@ def AckFileId(RecivedMassage):
     return PacketFileId
 
 
-# =========================================================================================
+""""# =========================================================================================
 def create_socket(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('127.0.0.1', port))
     return s
-
+"""""
 
 # =========================================================================================
 def save_data_as_image(data, output_file):
@@ -38,38 +38,49 @@ def Write_Data(filename, data_buffer):
 
 # =========================================================================================
 port = 9000
-r = create_socket(port)
+#r = create_socket(port)
 # Define the expected packet ID and create a buffer to store received data
 expected_packet_id = 0
 data_buffer = []
 # Define the window size
 window_size = 4
+W_array=[]
 filename = ''
+
+# Receive a packet
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('192.168.66.81', port))
 # Loop to receive packets
 while True:
-    # Receive a packet
-    packet, client_address = sock.recvfrom(1024)
-    # Parse the packet to extract the header and trailer information and the application data
-    packet_header = packet[:4]  # Assuming a 4-byte header
-    if AckId(packet_header) == 0:
-        file_id_initial=AckFileId(packet_header)
-    file_id = AckFileId(packet_header)
-    packet_data = packet[4:-4]  # Assuming a trailer of 4 bytes
-    packet_trailer = packet[-6:]  # Assuming a 4-byte trailer
-    # Extract the packet ID from the header
-    packet_id = AckId(packet_header)
-    # If the packet ID is the expected ID, store the data in the buffer
-    if packet_id == expected_packet_id and file_id==file_id_initial:
-        data_buffer.append(packet_data)
-        expected_packet_id += 1
-        sock.sendto(packet_header, client_address)
-    else:
-        print(f"Received out of order packet {packet_id}. Discarding data.")
-        if packet_trailer == b'\xff\xff':
-            Write_Data(filename, data_buffer)
-            print("File reception complete.")
+    s.settimeout(10)
+    packet_header = None
+    client_address=None
+    packet_id=None
+    try:
 
-# Write the received data to a file
+        while True:
+           packet, client_address = s.recvfrom(1024)
+           # Parse the packet to extract the header and trailer information and the application data
+           packet_header = packet[:4]  # Assuming a 4-byte header
+           if AckId(packet_header) == 0:
+               file_id_initial = AckFileId(packet_header)
+           file_id = AckFileId(packet_header)
+           packet_data = packet[4:-4]  # Assuming a trailer of 4 bytes
+           packet_trailer = packet[-6:]  # Assuming a 4-byte trailer
+           # Extract the packet ID from the header
+           packet_id = AckId(packet_header)
+           # If the packet ID is the expected ID, store the data in the buffer
+           if packet_id == expected_packet_id and file_id == file_id_initial:
+               print("recevied packet number ", packet_id)
+               data_buffer.append(packet_data)
+               expected_packet_id += 1
 
+           else:
+               print(f"Received out of order packet {packet_id}. Discarding data.")
+           if packet_trailer == b'\xff\xff':
+               Write_Data(filename, data_buffer)
+               print("File reception complete.")
 
-# Indicate via the user interface that the file reception is complete
+    except socket.timeout:
+        s.sendto(packet_header, client_address)
+        print("Ack packet number ", packet_id)
