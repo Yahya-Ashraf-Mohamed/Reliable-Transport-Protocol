5c0691a65b73c9f887f79289b7ad48e90f4703f4
Yahya Ashraf Mohamed <95289367+Yahya-Ashraf-Mohamed@users.noreply.github.com>
2023-04-30 20:15:33 +0200

    Updating Server V1.2

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ from queue import Queue
 import os
 
 
-# function to convert bytes to integer
-def bytes_to_int(b):
-    return int.from_bytes(b, byteorder='big')
+# function to convert integer to bytes
+def int_to_2bytes(num):
+
+    # Convert the integer to a 2-byte little-endian byte string
+    byte_str = num.to_bytes(2, byteorder='little', signed=False)
+
+    # Return the byte string as a bytes object
+    return bytes(byte_str)
+
 
 
 # =========================================================================================
@@ -75,44 +81,54 @@ def AddingHeadersToThePackets(picturepath, File_id):
 # =========================================================================================
 def create_socket(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('192.168.66.81', port))
     return s
 
 
 # =========================================================================================
 # function to send packets to receiver
-def send_packets_to_receiver(packets, window_size, timeout, file_id):
+def send_packets_to_receiver(packets, window_size, file_id):
     sock = create_socket(9000)
     unack_packets = packets[:window_size]
     expectedIds = [AckId(i) for i in unack_packets]
-    while unack_packets[0]:
+    lastAck=0
+    while packets[0]:
         # first [p(0),p(1),p(2),p(3)]
         #       [p1id,p2id,p3id,p4id]
         for i in range(len(unack_packets)):
             if i == 0:
-                sock.settimeout(timeout)
-            sock.sendto(unack_packets[i], ('192.168.66.104', 9000))
-            print("packet ",i)
+                sock.settimeout(21)
+            time.sleep(2)
+            sock.sendto(unack_packets[i], ('192.168.66.81', 9000))
+            print("packet ",AckId(unack_packets[i]))
         try:
             ack, addr = sock.recvfrom(1024)
+            print("ack recived",AckId(ack))
             received_ack_id = AckId(ack)
-            if received_ack_id in expectedIds and file_id == AckFileId(
-                    ack):  # if received_ack id within the expected id and the received file id is
+            ackk =ack[2:]
+            if (received_ack_id in expectedIds) and file_id == ackk:  # if received_ack id within the expected id and the received file id is
+                
                 # corrct
                 expectedIds = [i for i in range(received_ack_id + 1, 4 + (
                         received_ack_id + 1))]  # update the  list of expected ids depending on the received_one
-                for i in range(received_ack_id + 1):  # update the last of unacknowledged
-                    unack_packets.pop(i)
+                
+                for i in range(received_ack_id-lastAck):
+                              # update the last of unacknowledged
+                        unack_packets.pop()
+
                 if len(packets) > window_size:  # checking wither the packets contains less than the window size
                     for i in range(received_ack_id + 1, 4 + (received_ack_id + 1)):
+                        """print('rec id ',i)
+                        print('appendind packet',AckId(packets[i]))
+                        print(packets[i][:4])"""
                         unack_packets.append(packets[i])
                 else:
                     # the case when the remaining packets is less that the window size
                     for i in range(received_ack_id + 1, len(packets) + 1 + received_ack_id):
                         unack_packets.append(None)
-                packets = packets[received_ack_id + 1:]
+                lastAck=received_ack_id
 
         except socket.timeout:
+            print("استر يا رب")
             unack_packets = packets[:window_size]
 
 
@@ -128,14 +144,14 @@ def get_trailer_value(chunks, i):
 # =========================================================================================
 max_chunk_size = 1024  # maximum massage size
 window_size = 4  # sliding window size in go back N protocol
-File_id = bitesIntobytes(0, 16)
+File_id =bitesIntobytes(0, 16)
 flag = 'yes'
 while flag == 'yes':
-    File_name = 'SmallFile.png'
+    File_name = 'D:\Self Development\Zewail collage material\Academic years\Year 3\Semester 2\Computer Networks\Project\Reliable-Transport-Protocol\SmallFile.png'
     packets = AddingHeadersToThePackets(File_name, File_id)
-    send_packets_to_receiver(packets, window_size, 30, File_id)
-    File_id += 1
-    flag = input("Do you want to send another file")
+    send_packets_to_receiver(packets, window_size, File_id)
+    print(packets[8][:4])
+"""    File_id += 1
+    flag = input("Do you want to send another file")"""
 
 # function to get trailer value for a packet
-filename = 'C:\\Users\\fatma taha\\Desktop\\ZC\\Reliable-Transport-Protocol\\SmallFile.png'
